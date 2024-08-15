e126c22f4da778d0ecc3bde0da79fa2ef224f5c7
Turcan Cicek <turcan.cicek@ribbonhome.com>
2024-08-15 18:02:24 -0400

    Updates

---
+++ precache-manifest.db092a4423b96bbcbcf63589a18da8f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f66684e813494affa37f217ce09bc32",
+    "url": "/splash/index.html"
+  },
+  {
+    "revision": "5b5399334b10cae6e69a",
+    "url": "/splash/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "3d1fe5ab84f2b20c0886",
+    "url": "/splash/static/css/main.c7f123d8.chunk.css"
+  },
+  {
+    "revision": "5b5399334b10cae6e69a",
+    "url": "/splash/static/js/2.83cc57c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/splash/static/js/2.83cc57c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1fe5ab84f2b20c0886",
+    "url": "/splash/static/js/main.01f5e45b.chunk.js"
+  },
+  {
+    "revision": "15ad0b3c70fdc162cdb5",
+    "url": "/splash/static/js/runtime-main.37b0f58e.js"
+  }
+]);
